9ca14d51907d01315c149fac62a15d4288b1f386
Christhopher Lion <christhopherleao@icloud.com>
2019-05-18 17:04:00 +0100

    moved to React Static

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+        location: "../src",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { memo } from 'react'
+
+import './app.css'
+import logo from './logo.png'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        We are preparing sommething for you...
+      </p>
+      <span>
+        See more at&nbsp;
+        <a
+          className="App-link"
+          href="https://lion.alia.ml/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Christhopher Lion's profile
+        </a>
+      </span>
+    </header>
+  </div>
+)
+
+export default memo(App)
