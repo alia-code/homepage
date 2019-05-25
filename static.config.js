8aa2f04e7d9ab9b98732cc613ce6706adf08a6c5
Christhopher Lion <christhopherleao@icloud.com>
2019-05-25 16:03:17 +0100

    fix deps [skip ci]

--- static.config.js
+++ static.config.js
@@ -8,7 +8,7 @@
 export default {
   maxThreads: 1, // Remove this when you start doing any static generation
   plugins: [
-    "react-static-plugin-styled-components",
-    "react-static-plugin-sass",
+    'react-static-plugin-styled-components',
+    'react-static-plugin-sass',
   ],
-}
+};
