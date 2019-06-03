b73b8c05bd800a93ce1b4be8b1796f42c628d578
Christhopher Lion <christhopherleao@icloud.com>
2019-06-03 18:39:51 +0100

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = React.memo(() => (
       </Layout>
     </Container>
 
-    <PoweredBy />
+    {PoweredBy}
   </FooterStyled>
 ));
 
--- src/components/Footer/PoweredBy.jsx
+++ src/components/Footer/PoweredBy.jsx
@@ -9,17 +9,27 @@ const fontStack = `
   "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
 `;
 
-const PoweredBy = () => (
-  <p
-    className="p1"
+const PoweredBy = (
+  <div
     style={{
       fontFamily: fontStack,
       textAlign: 'center',
       fontSize: '.7rem',
     }}
   >
-  Powered by ΛLIΛ
-  </p>
+    <p className="p1">
+      Powered by
+      <a href="https://alia.ml">ΛLIΛ</a>
+    </p>
+    <p className="p1">
+      <a href="https://m.do.co/c/08d07343712a">
+        Click here
+      </a>
+       to subscribe to DigitalOcean and get
+      <b>50$</b>
+       credit
+    </p>
+  </div>
 );
 
 export default PoweredBy;
